ffcd24e1c41207e035104a957a72971a8a2dd3bc
zhuchenxi <973829691@qq.com>
2015-12-10 17:32:24 +0800

    修改分享地址

--- presentation/src/main/java/com/jude/emotionshow/presentation/setting/AboutPresenter.java
+++ presentation/src/main/java/com/jude/emotionshow/presentation/setting/AboutPresenter.java
@@ -9,6 +9,6 @@ import com.umeng.share.ShareManager;
 public class AboutPresenter extends Presenter<AboutActivity>{
     public void share(){
         String content = "么么秀是你最好的选择";
-        ShareManager.getInstance(getView()).share(getView(), content, "么么秀推荐", "http://baidu.com", "http://7xnrrg.com2.z0.glb.qiniucdn.com/logo72.png");
+        ShareManager.getInstance(getView()).share(getView(), content, "么么秀推荐", "http://114.215.86.90/mooshow/index.html", "http://7xnrrg.com2.z0.glb.qiniucdn.com/logo72.png");
     }
 }
